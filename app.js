e172afda872c8b12ea9950aa980e24934244b012
cktricky <cktricky@gmail.com>
2018-10-07 17:16:38 -0400

    completed some bits needed

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', isAuthenticated, usersRouter);
-//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +38,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Debug stuff
+app.get('/debug', function(req, res,next) {
+  res.send(process.env)
 });
 
 module.exports = app;
--- auth.js
+++ auth.js
@@ -1,3 +1,10 @@
 module.exports = async function isAuthenticated (req, res, next) {
+  /* TODO Make authentication
+   The above comment was purposefully added to demonstrate that
+   often you will find unfinished but important work when...
+   TODO, TO-DO, Hack, FIXME, FIX ME, etc. are left in source so...
+   SEARCH FOR IT!!! Also, this function is totally useless but wanted
+   students to at least trace and look at it.
+  */
   next();
 }
